c0cd3c2a75adadb63a0ad3364a2516e24ce70e1e
mlivebardo <tsunoll96@gmail.com>
2020-09-16 10:55:20 +0200

    Hello_Cylindre

--- TP1/Assets/Script/Helo_Sphere.cs
+++ TP1/Assets/Script/Helo_Sphere.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hello_Cercle : MonoBehaviour
+public class Helo_Sphere : MonoBehaviour
 {
-
     const float PI = 3.1415926f;
-    public int rayon, hauteur, nmeridien;
+    public int rayon, nparallele, nmeridien;
     public int[] triangles;
     public Vector3[] vertices;
     public Material mat;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -19,25 +17,50 @@ public class Hello_Cercle : MonoBehaviour
 
         // Création des structures de données qui accueilleront sommets et  triangles  // Remplissage de la structure sommet 
 
-        vertices = new Vector3[nmeridien + 1];
-        triangles = new int[nmeridien * 3];
+        vertices = new Vector3[(nmeridien +1) *(nparallele+1)];
+        triangles = new int[nmeridien * 2 *nparallele * 3];
 
 
-        vertices[nmeridien] = new Vector3(0, 0, 0);
+        int index = 0;
 
-        for (int i = 0; i < nmeridien; i++)
+        for (int i = 0; i <= nparallele; i++)
         {
-            vertices[i] = new Vector3(rayon * Mathf.Sin((2 * PI * i + 1) / nmeridien), 0, rayon * Mathf.Cos((2 * PI * i + 1) / nmeridien));
+            float phi = -PI / 2 + i * PI / nparallele;
+            for(int j = 0; j<= nmeridien; j++)
+            {
+                float teta = j * 2 * PI / nmeridien;
+                
+                float x = Mathf.Cos(teta) * Mathf.Cos(phi);
+                float y = Mathf.Sin(teta) * Mathf.Cos(phi);
+                float z = Mathf.Sin(phi);
 
+                vertices[index] = new Vector3(x, y ,z);
+                index++;
+            }
         }
+
         int k = 0;
-        for (int j = 0; j < nmeridien; j++)
+        for (int j = 0; j < nparallele; j++)
         {
-            triangles[k] = nmeridien;
-            triangles[k + 1] = (j);
-            triangles[k + 2] = ((j) + 1) % nmeridien;
-            k += 3;
+            int r1 = j * (nmeridien + 1);
+            int r2 = (j+1) * (nmeridien + 1);
+            for (int i = 0; i < nmeridien; i++)
+            {
+                triangles[k] = r1 + i;
+                triangles[k+1] = r2 + i +1;
+                triangles[k+2] = r2 + i;
+
+                triangles[k+3] = r1 + i;
+                triangles[k+4] = r1 + i + 1;
+                triangles[k+5] = r2 + i + 1;
+
+                k += 6;
+            }
         }
+
+
+
+
 
 
         // Remplissage de la structure triangle. Les sommets sont représentés par leurs indices
@@ -58,6 +81,7 @@ public class Hello_Cercle : MonoBehaviour
 
         gameObject.GetComponent<MeshFilter>().mesh = msh;           // Remplissage du Mesh et ajout du matériel
         gameObject.GetComponent<MeshRenderer>().material = mat;
+
     }
 
     // Update is called once per frame
